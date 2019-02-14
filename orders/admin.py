f40b0eb99f6585d134b94a8239b2672fd45e1f0f
Nerd-1 <ooyendyk@tuta.io>
2019-02-15 02:31:11 +1000

    Registered models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Customer, Side, Order, SideOrder, Sauce, Crust, Topping, Pizza, PizzaTopping, PizzaOrder, PizzaToppingAlteration
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Side)
+admin.site.register(Order)
+admin.site.register(SideOrder)
+admin.site.register(Sauce)
+admin.site.register(Crust)
+admin.site.register(Topping)
+admin.site.register(Pizza)
+admin.site.register(PizzaTopping)
+admin.site.register(PizzaOrder)
+admin.site.register(PizzaToppingAlteration)
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Customer
+
 # Create your views here.
 def index(request):
     return HttpResponse("Project 3: TODO")
