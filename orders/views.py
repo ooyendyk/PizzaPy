634028ed21911466bbde1ae02b3043eb88e66eea
Nerd-1 <ooyendyk@tuta.io>
2019-02-18 10:22:57 +0930

    Added registration, login, logout

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,30 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
 from .models import Customer
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html", {"message": None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "users/user.html", context)
+
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "users/login.html", {"message": "Invalid credentials."})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {"message": "Logged out."})
